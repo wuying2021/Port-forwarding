966119b26cf53ceb6a1677541dadc4b8bebe5a27
wuying2021 <85053993+wuying2021@users.noreply.github.com>
2024-03-10 00:44:04 +0800

    Update and rename RealmOneKey.sh to realmc.sh

--- realmc.sh
+++ realmc.sh
@@ -59,12 +59,13 @@ WantedBy=multi-user.target" > /etc/systemd/system/realm.service
 # 添加转发规则
 add_forward() {
     while true; do
-        read -p "请输入IP: " ip
-        read -p "请输入端口: " port
+        read -p "请输入监听端口: " lport
+        read -p "请输入落地IP: " rip
+        read -p "请输入落地端口: " rport
         # 追加到config.toml文件
         echo "[[endpoints]]
-listen = \"[::]:$port\"
-remote = \"$ip:$port\"" >> /root/realm/config.toml
+listen = \"[::]:$lport\"
+remote = \"$rip:$rport\"" >> /root/realm/config.toml
         
         read -p "是否继续添加(Y/N)? " answer
         if [[ $answer != "Y" && $answer != "y" ]]; then
