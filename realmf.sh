8b6d592c09da95951956a461e26d233d3060f02a
wuying2021 <85053993+wuying2021@users.noreply.github.com>
2024-03-10 00:58:08 +0800

    Update realmf.sh

--- realmf.sh
+++ realmf.sh
@@ -31,7 +31,7 @@ deploy_realm() {
     mkdir -p /root/realm
     cd /root/realm
     wget -O realm.tar.gz https://github.com/zhboner/realm/releases/download/v2.5.3/realm-x86_64-unknown-linux-gnu.tar.gz
-    if [-e /root/realm/realm]; then echo "文件存在"; else tar -xvf realm.tar.gz;fi
+    if [ -e /root/realm/realm]; then echo "文件存在"; else tar -xvf realm.tar.gz;fi
     chmod +x realm
     # 创建服务文件
     echo "[Unit]
